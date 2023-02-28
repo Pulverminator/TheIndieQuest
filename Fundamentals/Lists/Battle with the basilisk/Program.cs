e2fec723d88945a52d312f5f90fa4df014adf045
Pulverminator <Pulver_86@hotmail.com>
2023-02-28 20:23:24 +0100

    Add files via upload

--- Fundamentals/Lists/Battle with the basilisk/Program.cs
+++ Fundamentals/Lists/Battle with the basilisk/Program.cs
@@ -9,15 +9,10 @@ namespace Battle_with_the_basilisk
 		{
 			var random = new Random();
 			List<String> fighters = new List<String> { "Jazlyn", "Theron", "Dyana", "Rolando" };
-			List<int> fighterList = new List<int>();
-			foreach (string fighter in fighters)
-			{
-				fighterList.Add(random.Next(3, 8));
-			}
 			Console.WriteLine($"Fighters {string.Join(", ", fighters)} descend into the dungeon.");
-				// basilisk = 8d8+16 HP
+			// basilisk = 8d8+16 HP
 			int basiliskHP = 0;
-			for (int i = 0; i <= 8; i++) 
+			for (int i = 0; i <= 8; i++)
 			{
 				basiliskHP += random.Next(1, 9);
 			}
@@ -27,30 +22,59 @@ namespace Battle_with_the_basilisk
 
 			while (basiliskHP > 0 && fighters.Count > 0)
 			{
-				// greatsword = 2d6 damage
-				int greatsword = 0;
+				//Heroes attack
+				int dagger = 0;
 				foreach (string fighter in fighters)
 				{
 					for (int hit = 0; hit < 1; hit++)
 					{
-						greatsword += random.Next(1, 7) * 2;
+						dagger += random.Next(1, 5); //changed to 1d4 from 2d6
 					}
-					basiliskHP -= greatsword;
+					basiliskHP -= dagger;
 					if (basiliskHP < 0)
 					{
 						basiliskHP = 0;
 					}
-					Console.WriteLine($"{fighter} hits the basilisk for {greatsword} damage. Basilisk has {basiliskHP} left.");
-					greatsword = 0;
-					if (basiliskHP == 0) 
+					Console.WriteLine($"{fighter} hits the basilisk for {dagger} damage. Basilisk has {basiliskHP} left.");
+					dagger = 0;
+					if (basiliskHP == 0)
 					{
-						Console.WriteLine($"The basilisk collapses and the heroes celebrate their victory!");
 						break;
 					}
 				}
-			}
-			Console.ReadKey();
 
+				if (basiliskHP == 0)
+				{
+					break;
+				}
+
+				//Basilisk attack
+				int targetIndex = random.Next(fighters.Count);
+				string target = fighters[targetIndex];
+				Console.WriteLine($"The basilisk uses petrifying gaze on {target}!");
+
+				//DC 12 Constitution saving throw
+				int savingThrow = random.Next(1, 21);
+
+				if (savingThrow + 3 >= 12)
+				{
+					Console.WriteLine($"{target} rolls a {savingThrow} and is saved from the attack.");
+				}
+				else
+				{
+					fighters.Remove(target);
+					Console.WriteLine($"{target} rolls a {savingThrow} and is turned into stone.");
+				}
+			}
+
+			if (basiliskHP == 0)
+			{
+				Console.WriteLine($"The basilisk collapses and the heroes celebrate their victory!");
+			}
+			else
+			{
+				Console.WriteLine($"The party has failed and the basilisk continues to turn unsuspecting adventurers to stone.");
+			}
 		}
 	}
 }
