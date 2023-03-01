991d2f05818f60d203a487d4256a3ab0e395fd23
Pulverminator <Pulver_86@hotmail.com>
2023-03-01 10:52:08 +0100

    Add files via upload

---
+++ First Game/Unit 2/Dice and throw/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Unit2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var random = new Random();
+			int diceThrowOne = random.Next(1, 7);
+			int diceThrowTwo = random.Next(1, 7);
+			int diceThrowThree = random.Next(1, 7);
+			int totalScore = ((diceThrowOne + diceThrowTwo) + (diceThrowThree * 3)) * 2;
+			Console.WriteLine($"You roll: {diceThrowThree},{diceThrowTwo},{diceThrowThree}");
+			Console.WriteLine($"Your total score is: {totalScore}");
+		}
+	}
+}
