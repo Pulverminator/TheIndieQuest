34b71ea9752612e32618a9ec22e96c0e27f89d0a
Pulverminator <Pulver_86@hotmail.com>
2023-03-01 11:02:35 +0100

    Add files via upload

---
+++ First Game/Unit 3/Roll till six/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Roll_till_six
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var random = new Random();
+			int throwDice;
+			int score = 0;
+			do
+			{
+				throwDice = random.Next(1, 7);
+				Console.WriteLine($"Player rolls: {throwDice}");
+				score += throwDice;
+			} 
+			while (throwDice != 6);
+			Console.WriteLine($"Total score: {score}");
+		}
+	}
+}
