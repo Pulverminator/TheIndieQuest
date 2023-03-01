eec65ec7d3b235e8e3321b9eb0c00af01af79ea6
Pulverminator <Pulver_86@hotmail.com>
2023-03-01 10:53:14 +0100

    Add files via upload

---
+++ First Game/Unit 2/Distance to Alpha Centauri/Distance to Alpha Centauri/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Distance_to_Alpha_Centauri
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			double lightyearM = 4.365;
+			double parsecM = 0.30856775812799588;
+			double parsecs = lightyearM * parsecM;
+			Console.WriteLine($"The distance to Alpha Centauri is: {parsecs} parsecs");
+		}
+	}
+}
