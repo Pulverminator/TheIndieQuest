49ca45e30fd19a63ea30949d1f47195a0aa0dd66
Vic <Pulver_86@hotmail.com>
2023-03-07 16:33:14 +0100

    The Matrix

---
+++ Fundamentals/Algorithm/The Matrix/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace The_Matrix
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var streams = new List<int>;
+			var symbols = "!@#$%^&*()_+-=[];',.\/~{}:|<>?";
+
+		}
+	}
+}
