1f4615de5ec498f4b98e3f1c81fcd008327226f3
Vic <Pulver_86@hotmail.com>
2023-03-07 14:54:38 +0100

    Update

--- Fundamentals/Algorithm/Ordinal numbers/Program.cs
+++ Fundamentals/Algorithm/Ordinal numbers/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 
-namespace Ordinal_numbers
+namespace OrdinalNumber
 {
-	internal class Program
+	class Program
 	{
+		Random random = new Random();
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			OrdinalNumber();			
+		}
+
+		static string OrdinalNumber(int number)
+		{
+			Console.WriteLine(number);
+			int lastDigit = number % 10;
+			int secondToLastDigit = (number / 10) % 10;
+
+			if (secondToLastDigit == 1)
+			{
+				return number + "th";
+			}
+			if (lastDigit == 1)
+			{
+				return number + "st";
+			}
+			if (lastDigit == 2)
+			{
+				return number + "nd";
+			}
+			if (lastDigit == 3)
+			{
+				return number + "rd";
+			}
+			else
+			{
+				return number + "th";				
+			}
+			
+
 		}
 	}
 }
