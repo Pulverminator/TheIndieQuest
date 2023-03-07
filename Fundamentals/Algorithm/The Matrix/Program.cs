48ff8d058d87ec9fc32adafb7a0d94805db32d13
Vic <Pulver_86@hotmail.com>
2023-03-07 16:35:05 +0100

    Update Program.cs

--- Fundamentals/Algorithm/The Matrix/Program.cs
+++ Fundamentals/Algorithm/The Matrix/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace The_Matrix
 {
@@ -7,9 +8,29 @@ namespace The_Matrix
 	{
 		static void Main(string[] args)
 		{
-			var streams = new List<int>;
+			var streams = new List<int>
 			var symbols = "!@#$%^&*()_+-=[];',.\/~{}:|<>?";
 
+			for (i = 0; i < 10; i++) streams.Add(random.Next(0, 80));
+
+			Console.ForegroundColor = ConsoleColor.DarkGren;
+
+			while (true)
+			{
+				for (int x = 0; x < 80; x++)
+				{
+					Console.Write(streams.Contains(x) ? symbols[random.Next(symbols.Length)] : ' ';
+				}
+
+				Console.WriteLine();
+				Thread.Sleep(100)
+
+
+
+				if (random.Next(3) = 0) streams.RemoveAt(random.Next(streams.Count));
+				if (random.Next(3) = 0) streams.Add(random.Next(0, 80));
+
+			}
 		}
 	}
 }
