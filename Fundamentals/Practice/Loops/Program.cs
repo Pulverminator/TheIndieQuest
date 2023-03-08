a08cba61f1b68dd1415993f94e629a7b41046bfb
Vic <Pulver_86@hotmail.com>
2023-03-08 17:43:28 +0100

    Update Program.cs

--- Fundamentals/Practice/Loops/Program.cs
+++ Fundamentals/Practice/Loops/Program.cs
@@ -24,6 +24,18 @@ namespace HashTagObjects
 			Console.WriteLine("\nDrawing an isosceles triangle with height " + n);
 			DrawIsoscelesTriangle(n);
 
+			Console.WriteLine("\nDraw a square with side n with every other row blank. " + n);
+			DrawSquareOtherBlank(n);
+
+			Console.WriteLine("\nDraw a square with side n with every other column blank. " + n);
+			quareWithside(n);
+
+			Console.WriteLine("\nDraw a grid with side n with a line on every other row and every other column. " + n);
+			DrawNine(n);
+
+
+
+
 			Console.ReadLine();
 		}
 
@@ -45,7 +57,7 @@ namespace HashTagObjects
 
 		static void DrawRightTriangle(int n) // Draw a right triangle with legs size n.
 		{
-			for (int i = 0; i < n; i++)
+			for (int i = n; i >= 0; i--)
 			{
 				Console.WriteLine(new string('#', n - i));
 			}
@@ -55,7 +67,7 @@ namespace HashTagObjects
 		{
 			for (int i = 0; i < n; i++)
 			{
-				Console.WriteLine(new string('.', n - i) + new string('#', n));
+				Console.WriteLine(new string('.', n - i - 1) + new string('#', n));
 			}
 		}
 
@@ -63,17 +75,65 @@ namespace HashTagObjects
 		{
 			for (int i = 0; i < n; i++)
 			{
-				Console.WriteLine(new string('.', n - i - 1) + new string('#',  2 * i + 1));
+				Console.WriteLine(new string('.', n - i - 1) + new string('#', 2 * i + 1));
 			}
 		}
+		static void DrawSquareOtherBlank(int n) // Draw a square with side n with every other row blank.
+		{
+			for (int y = 0; y < n; y++)
+			{
+				for (int x = 0; x < n; x++)
+				{
+					if (y % 2 == 0)
+					{
+						Console.Write("#");  //#
+					}
+					else
+					{
+						Console.Write(".");  //.
 
+					}
+				}
+				Console.WriteLine();
+			}
+		}
+		static void quareWithside(int n) // Draw a square with side n with every other column blank.
+		{
+			for (int y = 0; y < n; y++)
+			{
+				for (int x = 0; x < n; x++)
+				{
+					if (x % 2 == 0)
+					{
+						Console.Write("#");  //#
+					}
+					else
+					{
+						Console.Write(".");  //.
 
-
-
-
-
-
-
-
+					}
+				}
+				Console.WriteLine();
+			}
+		}
+		static void DrawNine(int n) // Draw a grid with side n with a line on every other row and every other column.
+		{
+			for (int y = 0; y < n; y++)
+			{
+				for (int x = 0; x < n; x++)
+				{
+					if (y % 2 == 0)
+					{
+						Console.Write("#");  //#
+					}
+					else if (x % 2 == 1)
+					{
+						Console.Write(".");  //.
+					}
+				}
+				Console.WriteLine();
+			}
+		}
 	}
+
 }
