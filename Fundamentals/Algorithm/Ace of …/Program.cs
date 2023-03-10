3242d3c55199bac053793604eda14a24f1553f81
Vic <Pulver_86@hotmail.com>
2023-03-10 13:51:17 +0100

    Update Program.cs

--- Fundamentals/Algorithm/Ace of …/Program.cs
+++ Fundamentals/Algorithm/Ace of …/Program.cs
@@ -4,9 +4,25 @@ namespace Ace_of__
 {
 	internal class Program
 	{
+		enum Suit
+		{
+			Heart,
+			Spade,
+			Diamond,
+			Club
+		}
+
+		static void DrawAce()
+		{
+			Console.WriteLine("╭───────╮\r\n│A      │\r\n│♠      │\r\n│   ♠   │\r\n│      ♠│\r\n│      A│\r\n╰───────╯\r\n");
+		}
+
+
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+			DrawAce();
 		}
 	}
 }
