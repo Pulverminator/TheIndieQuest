da805e389e057a81aa8f4d90d586f5bbfd5f2357
Pulverminator <Pulver_86@hotmail.com>
2023-03-25 20:11:46 +0100

    Add files via upload

---
+++ Fundamentals/Algorithm/Arrays/Increasing level difficulty/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Increasing_level_difficulty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[100];
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 51);
+            }
+            
+            Array.Sort(array);
+            Console.WriteLine(("Number of monsters in levels: ") + (string.Join(", ", array)));
+        }
+    }
+}
