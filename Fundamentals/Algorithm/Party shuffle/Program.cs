b20be4072bfd7ad02ef661090c256836941cf378
Vic <Pulver_86@hotmail.com>
2023-03-27 11:37:40 +0200

    Party shuffle

--- Fundamentals/Algorithm/Party shuffle/Program.cs
+++ Fundamentals/Algorithm/Party shuffle/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Party_shuffle
 {
@@ -6,7 +7,42 @@ namespace Party_shuffle
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			List<string> names = new List<string>
+			{
+				"Allie",
+				"Ben",
+				"Claire",
+				"Dan",
+				"Eleanor"
+			};
+			PrintList(names);
+			Console.WriteLine("Signed-up participants: " + string.Join(", ", names));
+			Console.WriteLine();
+			Console.Write("Generating starting order ...");
+			ShuffleList(names);
+			Console.WriteLine();
+			Console.Write("\nStarting order: " + string.Join(", ", (names)));
+			Console.ReadKey();
+		}
+
+		static void PrintList(List<string> items)
+		{
+			foreach (string item in items)
+			{
+				Console.Write(item);
+			}
+		}
+
+		static void ShuffleList(List<string> items)
+		{
+			Random random = new Random();
+			for (int i = items.Count - 1; i > 0; i--)
+			{
+				int j = random.Next(i + 1);
+				string temp = items[i];
+				items[i] = items[j];
+				items[j] = temp;
+			}
 		}
 	}
 }
